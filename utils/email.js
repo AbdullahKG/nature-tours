ab6e9228f7270eaac312c824728592e5a3b4d2a1
AbdullahKG <abdullah.aboode.omar@gmail.com>
2024-11-08 00:31:01 +0300

    initial commit

---
+++ utils/email.js
@@ -0,0 +1,34 @@
+/*
+  this only use mailtrp for emails they are not real emails
+  use any email service you prefer 
+  i didn`t implement SendGrid for email
+  because SendGrid blocked my IP for some reason LOL
+*/
+
+const nodemailer = require('nodemailer');
+
+const sendEmail = async options => {
+  // 1) Create a transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+
+  // 2) Define the email options
+  const mailOptions = {
+    from: 'Abdullah Omar <hello@gamil.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+    // html:
+  };
+
+  // 3) Actually send the email
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
